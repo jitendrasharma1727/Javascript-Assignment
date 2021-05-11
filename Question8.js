fecff3feddce0215ff09742c16002410ea1a58f2
jitendra sharma <60726515+jitendrasharma1727@users.noreply.github.com>
2021-05-11 16:36:59 +0530

    Create Question8.js

---
+++ Question8.js
@@ -0,0 +1,11 @@
+for (var x=20; x<=30; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
+}

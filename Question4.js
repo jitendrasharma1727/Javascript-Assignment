e172d789d5b5d23917f42a8d7707c64757e1e51a
jitendra sharma <60726515+jitendrasharma1727@users.noreply.github.com>
2021-05-11 16:32:54 +0530

    Create Question4.js

---
+++ Question4.js
@@ -0,0 +1,16 @@
+		function check_numbers(x) 
+  {
+    
+        
+  	 if ( x >= 50 && x <= 99) 
+    {
+         console.log ( x+" is  between range 50-100" );
+    } 
+   else 
+    {
+         console.log (x + " is  between  not in range 50-100" );
+    }	
+  }	
+  
+  check_numbers(68);
+  check_numbers(25);
